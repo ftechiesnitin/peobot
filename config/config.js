4bcb12aacd21bf87f60fbbdaec1c64112928018a
Nitin Dolli <nitin.dolli@gmail.com>
2017-03-15 00:32:45 +0800

    config updated

--- config/config.js
+++ config/config.js
@@ -1,10 +1,20 @@
-const dotenv = require('dotenv');
+const builder = require('botbuilder');
+// const dotenv = require('dotenv');
 
 // Load environment variables from .env file
-dotenv.load();
+// dotenv.load();
 
 const config = module.exports = {};
 
 // App configuration
 config.env = process.env.APP_ENV;
 config.port = process.env.APP_PORT;
+// Connection to Microsoft Bot Framework
+config.botFramework = {
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
+};
+// API AI configs
+config.apiAi = {
+  clientToken: process.env.CLIENT_TOKEN
+};
