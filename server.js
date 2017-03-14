ab6926eff0db684f547b6c46ff5686e0f25b880b
Nitin Dolli <nitin.dolli@gmail.com>
2017-03-14 23:43:51 +0800

    server sample bot file added

--- server.js
+++ server.js
@@ -1,73 +1,40 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const bodyParser = require('body-parser');
+const restify = require('restify');
+const builder = require('botbuilder');
+const apiAiRecognizer = require('api-ai-recognizer');
 // Get configuration
 const cfg = require('./config/config.js');
-
-// handle cors
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-  res.header('Access-Control-Allow-Credentials', false);
-  next();
+// Bot Configs
+const connector = new builder.ChatConnector({
+  appId: cfg.botFramework.appId,
+  appPassword: cfg.botFramework.appPassword
 });
-// bodyparser settings
-app.use(bodyParser.json({ limit: '50mb', extended: true, type:'application/json' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, type:'application/x-www-form-urlencoding' }));
-app.use(bodyParser.json({ type: 'application/*+json' }));
-app.use(bodyParser.raw({ limit: '50mb' }));
+const bot = new builder.UniversalBot(connector);
+const recognizer = new apiAiRecognizer(cfg.apiAi.clientToken);
+const intents = new builder.IntentDialog({
+     recognizers: [recognizer]
+});
+// add intents to bot
+bot.dialog('/', intents);
 
+// Event when Message received
+intents.matches('greeting', function (session) {
+    return session.send("Hey there, I am Pune Eat outs, I am food lover. Want to know more about Pune Eat Out. Ask me.");
+});
 
+intents.matches('whatIsPeo', function (session) {
+    return session.send("PEO is PUNE EAT OUTS. A Food lover group for pune people. We find good places to eat and share it with people.");
+});
 
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(normalizePort(cfg.port));
-server.on('error', onError);
-server.on('listening', onListening);
-/**
- * Normalize a port into a number, string, or false.
- */
-function normalizePort(val) {
-  let port = parseInt(val, 10);
+intents.matches('whoStarted', function (session) {
+    return session.send("PEO was started by Avengers.");
+});
 
-  if (isNaN(port)) return val;
-  if (port >= 0) return port;
-
-  return false;
-}
-/**
- * Event listener for HTTP server "error" event.
- */
-function onError(error) {
-  if (error.syscall !== 'listen') throw error;
-
-  let bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-/**
- * Event listener for HTTP server "listening" event.
- */
-function onListening() {
-  let addr = server.address();
-  let bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-  console.log('Listening on: ' + bind);
-}
+// Server Init
+const server = restify.createServer();
+server.listen(cfg.port, function() {
+    console.log('Listening on ' + cfg.port);
+});
+server.post('/api/message', connector.listen());
+server.get('/test', function (req, res) {
+  res.send({'message': 'test api.'});
+});
