53b3fc5bcca4246e945e9fb55e9e00326b9897af
Nitin Dolli <nitin.dolli@gmail.com>
2017-03-15 22:02:52 +0800

    more intentions and replys added

--- server.js
+++ server.js
@@ -25,8 +25,36 @@ intents.matches('whatIsPeo', function (session) {
     return session.send("PEO is PUNE EAT OUTS. A Food lover group for pune people. We find good places to eat and share it with people.");
 });
 
-intents.matches('whoStarted', function (session) {
+intents.matches('whoStartedPeo', function (session) {
     return session.send("PEO was started by Avengers.");
+});
+
+intents.matches('peoCardInfo', function (session) {
+    return session.send("More about PEO card http://puneeatouts.in/privilege-card/. One card for one person :).");
+});
+// time to get the peo card
+intents.matches('moreAboutPeoCard', function (session) {
+    return session.send("The PEO cards for year 2017 will be announced on 1st April 2017. You can know the process then.");
+});
+
+intents.matches('peoCardOffers', function (session) {
+    return session.send("For current PEO (Privilege) CARD offers, you can visit http://puneeatouts.in/peo-privilege/. These offers will change for 2017-18.");
+});
+
+intents.matches('paymentOfPeoCard', function (session) {
+    return session.send("Good things take time :). Let's wait till 1st April and its not a April fool..");
+});
+
+intents.matches('friendsOfPeo', function (session) {
+    return session.send("Friends of PEO.");
+});
+
+intents.matches('whatIsPeoMeet', function (session) {
+    return session.send("PEO meet is a specially curated and organized gathering of PEO members. Only 3-5 meets happen in an year and its a fun of its kind to attend these. So far in last 12 years we have done 35 meets.");
+});
+
+intents.matches('aniRelation', function (session) {
+    return session.send("About Aniruddha patil.");
 });
 
 // Server Init
